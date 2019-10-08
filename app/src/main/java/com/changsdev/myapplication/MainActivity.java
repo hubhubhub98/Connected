e960d2eaee17cbea66f024eb5f92b4d2442aac0c
Younggyeong Kim <dudrud981126@naver.com>
2019-10-08 21:15:22 +0900

    탭 레이아웃 추가

---
+++ app/src/main/java/com/changsdev/myapplication/MainActivity.java
@@ -0,0 +1,34 @@
+package com.changsdev.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+import eu.long1.spacetablayout.SpaceTabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Fragment> fragments;
+    SpaceTabLayout spaceTabLayout;
+    ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.vp_pager);
+        spaceTabLayout = findViewById(R.id.tl_space_tabs);
+        fragments = new ArrayList<>();
+        fragments.add(new FriendListFragment());
+        fragments.add(new ChatListFragment());
+        fragments.add(new RandomChatFragment());
+        fragments.add(new SettingFragment());
+
+
+        spaceTabLayout.initialize(viewPager,getSupportFragmentManager(),fragments,savedInstanceState);
+    }
+}
