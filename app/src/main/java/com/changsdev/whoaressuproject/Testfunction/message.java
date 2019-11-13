a8e28e476e594aaf0dc0fd11daad1c0e43ba59a5
Younggyeong Kim <dudrud981126@naver.com>
2019-11-13 22:59:10 +0900

    파이어베이스 기능 테스트 클래스들 추가

---
+++ app/src/main/java/com/changsdev/whoaressuproject/Testfunction/message.java
@@ -0,0 +1,12 @@
+package com.changsdev.whoaressuproject.Testfunction;
+
+public class message {
+    String Sender;
+    String message;
+
+    message(String Sender, String message){
+        this.Sender=Sender;
+        this.message=message;
+    }
+
+}
